57131b37d813952af409fe896e77447648785e81
Supun Nethsara <supunmax663@gmail.com>
2025-12-15 23:34:05 +0530

    Fix for test

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
-    base: './',
+    plugins: [react()],
+    base: './', // production relative paths
+    server: {
+        proxy: {
+            // Development-only proxy for API requests
+            '/register': 'https://eproducttest.netlify.app',
+            '/login': 'https://eproducttest.netlify.app',
+            // add other API endpoints as needed
+        }
+    }
 })
